5ac73c6d7e97571230cba2d115d2122cdc688089
sugu2344 <suganesh234@gmail.com>
2025-01-18 12:36:47 +0530

    step4:server.js,config.js,env  string added

--- server.js
+++ server.js
@@ -1,1 +1,15 @@
-console.log("hello world");
+const app = require("./app");
+const mongoose = require("mongoose");
+
+const { MONGODB_URI, PORT } = require("./utils/config");
+mongoose
+  .connect(MONGODB_URI)
+  .then(() => {
+    console.log("connected to the DataBase");
+    app.listen(PORT, () => {
+      console.log(`server running on http://127.0.0.1:${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("Failed To connect To DataBase");
+  });
