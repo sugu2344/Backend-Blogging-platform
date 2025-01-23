bbcf808b944ecad90ac7d634582c457e30961349
sugu2344 <suganesh234@gmail.com>
2025-01-23 08:55:16 +0530

    dummy commit

--- models/blogmodel.js
+++ models/blogmodel.js
@@ -2,6 +2,7 @@ const mongoose = require("mongoose");
 
 const blogModelSchema = new mongoose.Schema({
   title: String,
+  author:String,
   description: String,
 });
 module.exports = mongoose.model("blog", blogModelSchema, "users");
