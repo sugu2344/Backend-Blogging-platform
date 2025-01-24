e1c45480bd3b03eb8c866c6a39b5e62c257e6866
sugu2344 <suganesh234@gmail.com>
2025-01-24 14:01:53 +0530

    user model  created

--- models/blogmodel.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-
-const blogModelSchema = new mongoose.Schema({
-  title: String,
-  author:String,
-  description: String,
-});
-module.exports = mongoose.model("blog", blogModelSchema, "users");
---
+++ models/userModel.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+
+const userModelSchema = new mongoose.Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    Password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+module.exports = mongoose.model("User", userModelSchema, "users");
