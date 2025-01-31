5a8607e87cf724368aee72b5988159396fbe14e9
sugu2344 <suganesh234@gmail.com>
2025-01-31 21:26:20 +0530

    post model created

---
+++ models/postModel.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+const postModelSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  content: { type: String, required: true },
+  author: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  tags: [{ type: String }],
+  categories: [{ type: String }],
+  status: { type: String, enum: ["draft", "published"], default: "draft" },
+  createdAt: { type: Date, default: Date.now },
+});
+module.exports = mongoose.model("Post", postModelSchema, "posts");
