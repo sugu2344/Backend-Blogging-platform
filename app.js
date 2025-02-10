8a4053adcb050108648be31ae4ca9a417201b9d0
sugu2344 <suganesh234@gmail.com>
2025-02-10 22:36:35 +0530

    cors changes

--- app.js
+++ app.js
@@ -12,7 +12,9 @@ const app = express();
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    
+    origin:["http://localhost:5173",
+     "https://mernblogging-app.netlify.app"], 
     credentials: true,
   })
 );
